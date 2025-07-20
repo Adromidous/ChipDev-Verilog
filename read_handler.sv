ee8ca422674ccc5ed565fb2dcbf04a6f50f4d28e
Adromidous <110305385+Adromidous@users.noreply.github.com>
2025-07-20 19:42:57 -0400

    Update read_handler.sv
    Added 2FF synchronizer and read address generation.

--- read_handler.sv
+++ read_handler.sv
@@ -1,15 +1,19 @@
-module read_hander #(
-	parameter WIDTH=4
+module read_handler #(
+	parameter PTR_WIDTH = 16,
 )	(
 
 	input logic clk,
 	input logic rstn,
-	input [WIDTH:0] rd_ptr, //INPUT - GRAY CODED
-	input [WIDTH:0] wr_ptr, //OUTPUT FROM 2FF SYNCHRONIZER - GRAY CODED
-	output logic empty
+	input [PTR_WIDTH:0] wr_ptr, //OUTPUT FROM 2FF SYNCHRONIZER - GRAY CODED
+	input increment,
+	output logic empty,
+	output logic [PTR_WIDTH:0] bin_rd_ptr,
+	output logic [PTR_WIDTH:0] gray_rd_ptr
 );
 
-always_ff@ (posedge clk, negedge rstn) begin
+reg [PTR_WIDTH:0] 2ff_read_ptr_sync [0:1];
+
+always_ff@ (posedge clk,negedge rstn) begin
 	if (~rstn) begin
 		empty <= 1'b0;
 	end else begin
@@ -17,6 +21,29 @@ always_ff@ (posedge clk, negedge rstn) begin
 	end
 end
 
+always_ff@ (posedge clk, negedge rstn) begin
+	if (~rstn) begin
+		2ff_read_ptr_sync <= 'b0;
+	end else begin
+		2ff_read_ptr_sync[1] <= 2ff_read_ptr_sync[0];
+		2ff_read_ptr_sync[0] <= gray_rd_ptr;
+	end
+end
 
+always_ff@ (posedge clk, negedge rstn) begin
+	if (~rstn) begin
+		bin_rd_ptr <= 'b0;
+	end else begin
+		if (increment)
+			bin_rd_ptr <= bin_rd_ptr + 1'b1;
+	end	
+end
+
+gray_code #(
+		.WIDTH(PTR_WIDTH)
+)	(
+		.binary_value(bin_rd_ptr),
+		.gray_value(gray_rd_ptr)
+);
 
 endmodule
